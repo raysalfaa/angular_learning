2f38f20082e94dd7cad772669fc27aed66bf3759
raysalfaa <av724523@gmail.com>
2025-02-27 12:16:00 +0530

    structural directive

--- Project1/src/app/app.component.ts
+++ Project1/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { AddEmployeeComponent } from './component/add-employee/add-employee.component';
 import { NewEmployeeComponent } from './component/new-employee/new-employee.component';
 import { DataBindingComponent } from './component/data-binding/data-binding.component';
+import { StructuralComponent } from './component/directive/structural/structural.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,AddEmployeeComponent,NewEmployeeComponent,DataBindingComponent],
+  imports: [RouterOutlet,StructuralComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Project1/src/app/component/directive/structural/structural.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-structural',
+  imports: [CommonModule,FormsModule],
+  templateUrl: './structural.component.html',
+  styleUrl: './structural.component.css'
+})
+export class StructuralComponent {
+  isVisible:boolean=true;
+
+  showText1(){
+    this.isVisible=true;
+  }
+  hideText1(){
+    this.isVisible=false;
+  }
+  isVisible2:boolean=true;
+  toggleText2(){
+    this.isVisible2=!this.isVisible2;
+  }
+  num1:String="";
+  num2:String="";
+
+  isChecked:boolean=false;
+  country:String="";
+  countries:String[]=["India","USA","UK","Australia","Canada"];
+
+  studentList:any[]=[
+    {id:1, name:"AAA", city:"bengulure", age:20, isActive:false},
+    {id:2, name:"BBB", city:"Mysure", age:22, isActive:true},
+    {id:3, name:"CCC", city:"Patna", age:23, isActive:false},
+    {id:4, name:"DDD", city:"Delhi", age:20, isActive:false},
+    {id:5, name:"EEE", city:"Chennai", age:24, isActive:true},
+    {id:6, name:"FFF", city:"Kolkata", age:21, isActive:true},
+    {id:7, name:"Aman", city:"Gaya", age:23, isActive:true},
+  ];
+  
+}
